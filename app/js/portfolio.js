2ddd83d8bc09aa59fa6cc1ac5887e0fb8c8a68e3
Denis <den.shklyarskij@bk.ru>
2022-01-22 02:57:45 +0300

    uploading filter app

---
+++ app/js/portfolio.js
@@ -0,0 +1,41 @@
+angular
+  .module("portfolioApp", [])
+  .controller("selectFilter", ($scope, $http) => {
+    $scope.selected = "All";
+
+    $http({
+      method: "GET",
+      url: "./allCategories.json",
+    }).then((res) => {
+      const { data } = res;
+
+      $scope.allItems = data;
+    });
+
+    $http({
+      method: "GET",
+      url: "./websites.json",
+    }).then((res) => {
+      const { data } = res;
+
+      $scope.webItems = data;
+    });
+
+    $http({
+      method: "GET",
+      url: "./flayers.json",
+    }).then((res) => {
+      const { data } = res;
+
+      $scope.flayerItems = data;
+    });
+
+    $http({
+      method: "GET",
+      url: "./businessCategories.json",
+    }).then((res) => {
+      const { data } = res;
+
+      $scope.businessItems = data;
+    });
+  });
